be4d89a3edf4fcc535ede9edc38b1af5c44cb327
viniciuscsc <viniciuscsc@icloud.com>
2023-11-28 19:38:46 -0300

    Req 06: cria a funcao tradutor() em language_controller

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 
 language_controller = Blueprint("language_controller", __name__)
@@ -13,4 +14,24 @@ def index():
         translate_from="pt",
         translate_to="en",
         translated="What do you want to translate?",
+    )
+
+
+@language_controller.route("/", methods=["POST"])
+def tradutor():
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+
+    traducao = GoogleTranslator(translate_from, translate_to).translate(
+        text_to_translate
+    )
+
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=traducao,
     )
